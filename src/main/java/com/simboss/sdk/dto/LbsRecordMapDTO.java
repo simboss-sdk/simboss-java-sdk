0679bbe109d65808147053abc826ab927d4c6bb2
1207589433@qq.com <1207589433@qq.com>
2022-09-01 14:36:43 +0800

    添加getset

--- src/main/java/com/simboss/sdk/dto/LbsRecordMapDTO.java
+++ src/main/java/com/simboss/sdk/dto/LbsRecordMapDTO.java
@@ -14,4 +14,20 @@ public class LbsRecordMapDTO {
   private PageDTO page;
 
   private List<LbsRecordDetailDTO> list;
+
+  public PageDTO getPage() {
+    return page;
+  }
+
+  public void setPage(PageDTO page) {
+    this.page = page;
+  }
+
+  public List<LbsRecordDetailDTO> getList() {
+    return list;
+  }
+
+  public void setList(List<LbsRecordDetailDTO> list) {
+    this.list = list;
+  }
 }
