953a6466807c8c116a9993bca1d11e6f6f47cf9a
1207589433@qq.com <1207589433@qq.com>
2022-09-01 14:09:15 +0800

    bugfix

--- src/main/java/com/simboss/sdk/dto/LocationDTO.java
+++ src/main/java/com/simboss/sdk/dto/LocationDTO.java
@@ -14,4 +14,44 @@ public class LocationDTO {
   private String coordinate;
 
   private String location;
+
+  public String getIccid() {
+    return iccid;
+  }
+
+  public void setIccid(String iccid) {
+    this.iccid = iccid;
+  }
+
+  public String getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(String longitude) {
+    this.longitude = longitude;
+  }
+
+  public String getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(String latitude) {
+    this.latitude = latitude;
+  }
+
+  public String getCoordinate() {
+    return coordinate;
+  }
+
+  public void setCoordinate(String coordinate) {
+    this.coordinate = coordinate;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
 }
